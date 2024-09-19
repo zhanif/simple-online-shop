a6001314773acfcf27457bfd68e8c5ec6d05b146
zhanif <ihsanfauzan76@gmail.com>
2024-09-19 16:38:19 +0700

    implement main screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    NavbarComponent,
+    CommonModule,
     RouterOutlet,
-    FooterComponent
+    RouterLink,
+    NavbarComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online-shop';
+  isMainRoute = true
+
+  constructor (
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isMainRoute = (this.router.url == '/' || this.router.url == '')
+    })
+  }
 }
